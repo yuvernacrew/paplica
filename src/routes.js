b801fc2f6280f75c7479f74d0790a141de75f1e5
加藤　優奈 <yuvernacrew@gmail.com>
2018-04-21 20:51:23 +0900

    update: edit routing

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,9 @@
 import HomePage from './pages/home.vue';
 import AboutPage from './pages/about.vue';
-import FormPage from './pages/form.vue';
-import DynamicRoutePage from './pages/dynamic-route.vue';
-import NotFoundPage from './pages/not-found.vue';
-
-import PanelLeftPage from './pages/panel-left.vue';
-import PanelRightPage from './pages/panel-right.vue';
+import SchedulePage from './pages/schedule.vue';
+import MemberPage from './pages/member.vue';
+import ContactPage from './pages/contact.vue';
+import SettingPage from './pages/setting.vue';
 
 export default [
   {
@@ -13,27 +11,23 @@ export default [
     component: HomePage,
   },
   {
-    path: '/panel-left/',
-    component: PanelLeftPage,
-  },
-  {
-    path: '/panel-right/',
-    component: PanelRightPage,
-  },
-  {
     path: '/about/',
     component: AboutPage,
   },
   {
-    path: '/form/',
-    component: FormPage,
+    path: '/schedule/',
+    component: SchedulePage,
   },
   {
-    path: '/dynamic-route/blog/:blogId/post/:postId/',
-    component: DynamicRoutePage,
+    path: '/member/',
+    component: MemberPage,
   },
   {
-    path: '(.*)',
-    component: NotFoundPage,
+    path: '/contact/',
+    component: ContactPage,
   },
+  {
+    path: '/setting/',
+    component: SettingPage,
+  }
 ];
