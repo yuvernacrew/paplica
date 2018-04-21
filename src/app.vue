9a2a18d5a44717ff851c39f3d81bceb42c0fc2ee
加藤　優奈 <yuvernacrew@gmail.com>
2018-04-21 21:14:44 +0900

    update: edit icons

--- src/app.vue
+++ src/app.vue
@@ -8,14 +8,12 @@
     </f7-navbar>
 
     <f7-toolbar tabbar labels>
-      <f7-link icon="icon-home" material="home" text="about" tab-link="/about"></f7-link>
-      <f7-link icon="icon-2" text="schedule" tab-link="/schedule"></f7-link>
-      <f7-link icon="icon-2" text="member" tab-link="/member"></f7-link>
-      <f7-link icon="icon-2" text="contact" tab-link="/contact"></f7-link>
-      <f7-link icon="settings" text="setting" tab-link="/setting"></f7-link>
+      <f7-link icon="icon-about" text="about" tab-link="/about/"></f7-link>
+      <f7-link icon="icon-schedule" text="schedule" tab-link="/schedule/"></f7-link>
+      <f7-link icon="icon-member" text="member" tab-link="/member/"></f7-link>
+      <f7-link icon="icon-contact" text="contact" tab-link="/contact/"></f7-link>
+      <f7-link icon="icon-setting" text="setting" tab-link="/setting/"></f7-link>
     </f7-toolbar>
-
-
   </div>
 
 </template>
